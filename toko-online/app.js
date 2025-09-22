6d50ad2933560b5a036aa2241ee233b5259725a9
fery setiawan <ferysetiawan_2226240137@mhs.mdp.ac.id>
2025-09-22 17:06:47 +0700

    pertemuan 4 (update)
    update toko online

--- toko-online/app.js
+++ toko-online/app.js
@@ -6,12 +6,14 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var engine = require('ejs-blocks');//menggunakan ejs block
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+var engine = ('ejs', this.engine); //daftarkan engine ejs block
 app.set('view engine', 'ejs');
+
 
 app.use(logger('dev'));
 app.use(express.json());
