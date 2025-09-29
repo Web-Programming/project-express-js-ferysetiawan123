0ef7b37838074f960293b19732faae6f30232e53
ferysetiawan123 <ferysetiawan_2226240137@mhs.mdp.ac.id>
2025-09-29 15:56:03 +0700

    lanjutan toko-online
    pencarian produk

--- toko-online/routes/index.js
+++ toko-online/routes/index.js
@@ -10,4 +10,26 @@ router.get('/', function(req, res, next) {
   });
 });
 
+/* GET search product. */
+router.get('/search', function(req, res, next) {
+  const q = req.query.q ? req.query.q.toLowerCase() : "";
+
+  console.log("Search query:", q);
+
+  // filtering product
+  let filtered = products;
+  if (q) {
+    filtered = products.filter(p => p.name.toLowerCase().includes(q));
+  }
+
+  res.render('index', { 
+    title: 'Hasil Pencarian', 
+    products: filtered, 
+    query: req.query.q || "" 
+  });
+});
+
+console.log("Route index loaded");
+
+
 module.exports = router;
